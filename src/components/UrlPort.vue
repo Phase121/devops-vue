819aa44f067230da76843fe1415e99f2d3f858c2
Hi <admin@local>
2024-09-04 11:05:15 -0700

    Initialize...

---
+++ src/components/UrlPort.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>URL: {{ currentUrl }}</h3>
+    <h3>Port: {{ currentPort }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentUrl: '',
+      currentPort: ''
+    };
+  },
+  mounted() {
+    this.currentUrl = window.location.href; // Get the full URL
+    this.currentPort = window.location.port; // Get the port
+  }
+};
+</script>
+
+<style scoped>
+/* Add any styles you want here */
+</style>
